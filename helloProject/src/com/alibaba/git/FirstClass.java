da5e3fb7ac07e64333f5d1d4fa73f92f65314834
zjl <13867521410@163.com>
2018-10-02 15:59:51 +0800

    将工程添加到本地仓库

---
+++ helloProject/src/com/alibaba/git/FirstClass.java
@@ -0,0 +1,12 @@
+package com.alibaba.git;
+
+public class FirstClass {
+
+    public static void main(String[] args) {
+        System.out.println("hello git , i love you !");
+        System.out.println("hello git , i love you !");
+        System.out.println("hello git , i love you !");
+        System.out.println("hello git , i love you !");
+        System.out.println("hello git , i love you !");
+    }
+}
